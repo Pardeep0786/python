d7bc9b208f54393ad286bee96467d7fe6371379b
Pardeep0786 <pardeepkumar224678@gmail.com>
2024-07-04 01:18:20 -0700

    Add files via upload

---
+++ Python.py
@@ -0,0 +1,94 @@
+#write a program to print hello universe in python
+print("hello universe")
+
+
+#write a program for performing some mathematical operations
+a = 10 
+b = 20
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+
+#write a program for performing some logical operations
+a = 30
+b = 40
+print(a>b)
+print(a<b)
+print(a<=b)
+print(a>=b)
+
+#write a program to create a list 
+list = ["python",5,8,7,6,4]
+print(list)
+list.append(8)
+print(list)
+
+#special operator (in)
+T1=[5,6,8,7,2]
+print(5 in T1)
+
+#Mathematical functions
+import math
+print(math.sqrt(16))
+
+#conditional statement (if-else)
+a = 10
+b = 25
+if a>b:
+    print("a is greater")
+else:
+   print("b is greater")
+
+#iterative statement
+l1=[5,8,7,6,9,2]
+i = 0
+for i in l1:
+    print(i)
+
+#write a program to print the element of the string 
+s1=["python","java","c++","js"]
+print(s1)    
+   
+
+#Access the character of a string by index
+s2="python"
+print(s2[3])
+print(s2[0])
+print(s2[1])
+
+#Access the character of a string by slicing
+s3="python"
+print(s3[0:3])
+print(s3[1:4])
+print(s3[2:5])
+
+#Mathematical opertions for string (*,+)
+s4="python"
+s5="\tlanguage"
+print(s4 + s5)
+
+s6="python is a programming language"
+print(s6 * 4)
+
+#membership operator
+s7="python"
+print('o' in s7)
+print('s' not in s7)
+
+#compare two string
+s8="python"
+s9="javascript"
+print(s8 == s9)
+
+
+
+
+
+
+
+
+
+
+
+
